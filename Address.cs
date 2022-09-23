45d8e8a9c1ab8e8bc49a63768828f99a3ba8ff3f
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-09-23 14:43:15 +0200

    Aggiungere i file di progetto.

---
+++ Address.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Address
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public int Zip { get; set; }
+
+    public Address(string name, string surname, string street, string city, string province, int zip)
+    {
+        Name = name;
+        Surname = surname;
+        Street = street;
+        City = city;
+        Province = province;
+        Zip = zip;
+    }
+    //public Address() { }
+
+    public override string ToString()
+    {
+        string text = "";
+        text += $"Name: {Name} \nSurname: {Surname} \nStreet: {Street} \nCity: {City}  \nProvince: {Province} \nZip: {Zip}";
+        return text;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+StreamReader file = File.OpenText("addresses.csv");
+string riga = file.ReadLine(); // scarto la prima riga
+
+while (!file.EndOfStream)
+{
+     riga = file.ReadLine();
+    string[] split = riga.Split(',');
+    if (split.Count() == 6)
+    {
+        Address a = new Address(split[0], split[1], split[2], split[3], split[4], Convert.ToInt32(split[5]));
+        Console.WriteLine(a.ToString());
+        Console.WriteLine("---------\n---------");
+    }
+
+}
+ file.Close();
