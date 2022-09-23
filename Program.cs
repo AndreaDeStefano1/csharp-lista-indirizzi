4c7a4f81c3af48a8d8412f037390b4ee1f274d1d
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-09-23 15:30:25 +0200

    PPrima stampa

--- Program.cs
+++ Program.cs
@@ -7,14 +7,30 @@ string riga = file.ReadLine(); // scarto la prima riga
 
 while (!file.EndOfStream)
 {
-     riga = file.ReadLine();
+    riga = file.ReadLine();
     string[] split = riga.Split(',');
-    if (split.Count() == 6)
+
+    try
     {
-        Address a = new Address(split[0], split[1], split[2], split[3], split[4], Convert.ToInt32(split[5]));
+        string name = split[0];
+        string surname = split[1];
+        string street = split[2];
+        string city = split[3];
+        string province = split[4];
+        string zip = split[5];
+        Address a = new Address(name, surname, street,city,province,zip);
         Console.WriteLine(a.ToString());
         Console.WriteLine("---------\n---------");
+    }catch(Exception e)
+    {
+        Console.WriteLine(e.ToString());
     }
+    finally
+    {
+        file.Close();
+    }
+       
+        
+    
 
 }
- file.Close();
